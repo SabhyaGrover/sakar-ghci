a73ed2a8a1f173495bf2d0e25a38b47ee3027686
chanchal <abhay.porsa@gmail.com>
2020-08-25 01:28:41 +0530

    SignUp changes

--- screens/index.js
+++ screens/index.js
@@ -1,1 +1,24 @@
-export { default as SignUp } from "./screens/SignUp.js";
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+import MaterialCardBasic from "./components/MaterialCardBasic";
+
+function Index(props) {
+  return (
+    <View style={styles.container}>
+      <MaterialCardBasic style={styles.materialCardBasic}></MaterialCardBasic>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 359,
+    height: 282
+  },
+  materialCardBasic: {
+    height: 282,
+    width: 359
+  }
+});
+
+export default Index;
