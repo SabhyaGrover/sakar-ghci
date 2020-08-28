c25b56187e0f114e8d8b262acf99b17f701c57cf
chanchal <abhay.porsa@gmail.com>
2020-08-29 01:41:11 +0530

    changes added

--- App.js
+++ App.js
@@ -2,11 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
-import Welcome from './screens/Welcome';
+
 import login from './screens/login';
 import footer from './screens/footer';
 import SignUp from './screens/SignUp';
-import Index from './screens/index';
 import Interests from './screens/Interests';
 import { NavigationContainer } from '@react-navigation/native';
 import { render } from 'react-dom';
@@ -17,10 +16,10 @@ export default function App() {
   return (       
     <NavigationContainer>
       <Stack.Navigator>
+      
+      <Stack.Screen name = 'Interest' component = { Interests } />
         <Stack.Screen name = 'login' component = { login } />
-        <Stack.Screen name = 'SignUp' component = { SignUp } />
-        <Stack.Screen name = 'Todo' component ={ Todo } />
-        <Stack.Screen name = 'Interest' component = { Interests } />
+        <Stack.Screen name = 'SignUp' component = { SignUp } />       
         <Stack.Screen name = 'footer' component ={ footer} />
       </Stack.Navigator>
     </NavigationContainer>
