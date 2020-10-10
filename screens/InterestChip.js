0a38bf8c5b18000f5343d41cd4bf07a19b0f5eb9
chanchal <abhay.porsa@gmail.com>
2020-10-10 17:41:15 +0530

    Chip changes added

--- screens/InterestChip.js
+++ screens/InterestChip.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Chip } from 'react-native-paper';
 
 const MyComponent = () => (
-  <Chip icon="information" styles = {{marginTop:100}} onPress={() => console.log('Pressed')}>Example Chip</Chip>
+  <Chip icon="information" style = {{marginTop:100}} onPress={() => console.log('Pressed')}>Example Chip</Chip>
 );
 
 export default MyComponent;
